ca4536b648aa192977aeb5ce7e019737bb9a7fd6
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-07-23 19:21:48 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -23,3 +23,25 @@ function calculate(a: number, b: number, type: string): number {
 
 const ans = calculate(1,2,"sum");
 console.log(ans);
+
+// 3 //
+// if i could specify that u can pass only sum, sub, mul, div
+
+function calculate(a: number, b: number, type: "sum" | "sub" | "mul" | "div"): number {    // a nicer way to mention custom type
+  if(type === "sum"){
+  return a+b ;
+  }
+  if(type === "sub"){
+  return a-b ;
+  }
+  if(type === "mul"){
+  return a*b ;
+  }
+  if(type === "div"){
+  return ab /
+  }
+  return -1; // to handle the situation if function call goes wrong
+}
+
+const ans = calculate(1,2,"sum");
+console.log(ans);
