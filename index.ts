08f384b02315700f19fd75aec9bc8e8253f7872e
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-07-23 19:19:30 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,25 @@
-// Ts is useful in sending the right and recieving the right type of arguments
-function calculateSum(a: number, b: number):number{
+// Ts is useful in sending the right and recieving the right 'type' of arguments
+// 1 //
+function calculateSum(a: number, b: number): number{     //  s whitespace after: is important
   return a+b ;
 }
+
+// 2 //
+function calculate(a: number, b: number, type: string): number {
+  if(type === "sum"){
+  return a+b ;
+  }
+  if(type === "sub"){
+  return a-b ;
+  }
+  if(type === "mul"){
+  return a*b ;
+  }
+  if(type === "div"){
+  return ab /
+  }
+  return -1; // to handle the situation if function call goes wrong
+}
+
+const ans = calculate(1,2,"sum");
+console.log(ans);
