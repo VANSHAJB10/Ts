da4d53c5c4265629f8234b87af37687430002da9
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-08-19 23:57:31 +0530

    Update theory.ts

--- theory.ts
+++ theory.ts
@@ -8,6 +8,16 @@ const names: string[] = [];
 names.push("Github@Vb10"); // no error
 // names.push(5); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
 
+const names: readonly string[] = ["Dylan"];
+names.push("Jack"); // Error: Property 'push' does not exist on type 'readonly string[]'.
+// The readonly keyword can prevent arrays from being changed.
+
+const numbers = [1, 2, 3]; // inferred to type number[]
+numbers.push(4); // no error
+// comment line below out to see the successful assignment
+numbers.push("2"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+let head: number = numbers[0]; // no error
+//TypeScript can infer the type of an array if it has values.
 
 
 
